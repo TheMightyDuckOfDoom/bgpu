efdb407116d82e2fe1f872dc992b3292f1085b37
Tobias Senti <git@tsenti.li>
2025-03-26 22:46:20 +0100

    [sweep] only print on fail

--- sweep.sh
+++ sweep.sh
@@ -5,19 +5,19 @@ WaitBufferSizePerWarp_range=(2 4 8)
 
 for NumWarp in ${NumWarps_range[@]}
 do
-    echo "NumWarp: $NumWarp"
     for WaitBufferSizePerWarp in ${WaitBufferSizePerWarp_range[@]}
     do
         InflightInstructionsPerWarp=$((2 * WaitBufferSizePerWarp))
-        echo "NumWarp: $NumWarp, WaitBufferSizePerWarp: $WaitBufferSizePerWarp InflightInstructionsPerWarp: $InflightInstructionsPerWarp"
-        make tb_compute_unit VERILATOR_ARGS="-GNumWarps=$NumWarp -GWaitBufferSizePerWarp=$WaitBufferSizePerWarp -GInflightInstrPerWarp=$InflightInstructionsPerWarp"
+        result=$(make tb_compute_unit VERILATOR_ARGS="-GNumWarps=$NumWarp -GWaitBufferSizePerWarp=$WaitBufferSizePerWarp -GInflightInstrPerWarp=$InflightInstructionsPerWarp -GMaxSimCycles=2000")
         if [ $? -eq 0 ]; then
             echo "Simulation passed for NumWarp: $NumWarp, WaitBufferSizePerWarp: $WaitBufferSizePerWarp InflightInstructionsPerWarp: $InflightInstructionsPerWarp"
         else
+            echo "$result"
             echo "Simulation failed for NumWarp: $NumWarp, WaitBufferSizePerWarp: $WaitBufferSizePerWarp InflightInstructionsPerWarp: $InflightInstructionsPerWarp"
             exit 1
         fi
     done
+    echo ""
 done
 
 echo "All simulations passed"
