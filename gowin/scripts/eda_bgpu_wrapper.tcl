127a37594e76abc571596f6873233baf19d152e1
Tobias Senti <git@tsenti.li>
2025-08-06 14:55:26 +0200

    gowin: moved check_warnings to seperate script

--- gowin/scripts/eda_bgpu_wrapper.tcl
+++ gowin/scripts/eda_bgpu_wrapper.tcl
@@ -21,31 +21,4 @@ add_file ../../src/bgpu_wrapper.sv
 run syn
 
 # Check for synthesis errors
-set error 0
-proc check_for_warning {warning_as_error} {
-    global top_design
-    global error
-
-    set fp [open impl/gwsynthesis/${top_design}.log]
-    set filecontent [read $fp]
-    set input_list [split $filecontent "\n"]
-    set textlist [lsearch -all -inline $input_list "*${warning_as_error}*"]
-    set error 0
-    foreach elem $textlist {
-        puts $elem
-        set error 1
-    }
-    close $fp
-}
-
-check_for_warning "DI0002"
-check_for_warning "EX0200"
-check_for_warning "EX0201"
-check_for_warning "EX0205"
-check_for_warning "EX0206"
-check_for_warning "EX0210"
-
-if {$error} {
-    puts "Error: Warnings that should be errors found in synthesis log, aborting."
-    exit 2
-}
+source ../../scripts/eda_check_warnings.tcl
